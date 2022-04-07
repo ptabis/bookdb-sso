d0eb03314bc17648a341c60fc8787b08ddd5ea0b
Patryk Tabiś <p.tabis@protonmail.com>
2022-04-07 19:18:05 +0200

    authentication beggining

--- app/routers/default.py
+++ app/routers/default.py
@@ -1,14 +1,16 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Cookie, Depends, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.responses import JSONResponse, HTMLResponse, RedirectResponse
 from app.classes.UserClass import User
 from app.classes.TokenClass import Token
 from app.models.TokenModel import TokenResponse
 from app.models.RegisterModel import RegisterResponse
 from app.utils.basedir import BASE_DIR
+from app.utils.config import config
 from pathlib import Path
+from typing import Optional
 
 router = APIRouter()
 
@@ -17,7 +19,7 @@ templates = Jinja2Templates(directory=str(Path(BASE_DIR, "templates")))
 
 
 @router.get("/", response_class=HTMLResponse)
-def home(request: Request):
+def home(request: Request, site: Optional[str] = None):
     return templates.TemplateResponse("login.html.jinja", {"request": request})
 
 
@@ -37,6 +39,18 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
         response = JSONResponse()
 
     return response
+
+
+@router.get("/authenticate")
+def authenticate(
+    request: Request, access_token: Optional[str] = Cookie(None), site: str = None
+):
+    referer = request.headers.get("referer")
+
+    if referer == site and referer in config.hosts_whitelist:
+        return RedirectResponse(f"{referer}/auth?token={access_token}")
+
+    return RedirectResponse(f"/?site={referer}")
 
 
 @router.get("/register", response_class=HTMLResponse)
